3367126b20e3c8209c43f2b7784e8b6b78099e8e
Pekka Lehtola <pekka.lehtola98@gmail.com>
2021-01-11 14:09:29 +0200

    Updated tasks 1, 2, 3 to follow style guide.

--- Excercise1/Int_and_Str_lst.py
+++ Excercise1/Int_and_Str_lst.py
@@ -1,5 +1,16 @@
+#File name: Int_and_Str_lst
+#Author: Pekka Lehtola
+#Description: Prints a list of numbers inserted by user, prints a list of strings inserted by user and
+#             prints number list filled with random numbers.
+
+#importing random
+import random
+
+#creating the two lists.
 intlst = []
 strlst = []
+
+#appending lists by user inputs.
 for i in range(10):
     x = int(input("Insert an integer: "))
     intlst.append(x)
@@ -7,11 +18,11 @@ for i in range(10):
     y = str(input("Insert a string: "))
     strlst.append(y)
 
+#printing lists
 print(intlst)
 print(strlst)
 
-import random
-
+#Replaces numbers in list with random numbers between 0 and 100
 for i in range(len(intlst)):
     intlst[i] = random.randint(0,100)
 
