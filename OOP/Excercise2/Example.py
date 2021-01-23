cf2470928455418d2d6f42983626e12d2cd4fbf1
Pekka Lehtola <pekka.lehtola98@gmail.com>
2021-01-23 06:07:10 +0200

    Modified example exercise.

--- OOP/Excercise2/Example.py
+++ OOP/Excercise2/Example.py
@@ -10,19 +10,32 @@ class Coin:
     def __init__(self):
         self.sideup ="Heads"
 
-    # The toss method generate a random number
-    # int the range of 0 trough 1, If the number 0, the sideup is set to "Heads"
-    #otherwise, sideup is set to "Tails"
+
+    #Selects a random integer between 0 and 14. If the value is 0-4 side up is heads.
+    #If the value is 5-9 tails is choosen. 10-12 coin lands upright.
+    #13-14 coin falls into an rabbit hole...
 
     def toss(self):
-        if random.randint(0, 1) == 0:
+
+        random_number = random.randint(0,14)
+
+        if random_number in range(0,5):
             self.sideup = "Heads"
-        else:
+
+        elif random_number in range(5,10):
             self.sideup = "Tails"
+
+        elif random_number in range(10, 13):
+            self.sideup = "Coin landed upright"
+
+        else:
+            print("Coin landed into a rabbit hole...Game over, because you don't have a coin anymore.")
+            exit()
 
     # The get_sideup method return the value referenced by sideup.
 
     def get_sideup(self):
+
         return self.sideup
 
 # The main function
@@ -35,12 +48,19 @@ def main():
     # Display the side of the coin that is facing up.
     print("This side is up: ", my_coin.get_sideup())
 
-    #Toss the coin.
-    print("I am tossing the coin...")
-    my_coin.toss()
+    print("To toss the coin press enter.", end="\n\n")
 
-    # Display the side of the coin that is facing up.
-    print("This side is up: ", my_coin.get_sideup())
+    while True:
+
+        #Makes the user press enter before next toss.
+        user_input = input()
+
+        #Toss the coin.
+        print("I am tossing the coin...")
+        my_coin.toss()
+
+        # Display the side of the coin that is facing up.
+        print("This side is up: ", my_coin.get_sideup())
 
 
 # Call the main function.
