9e07bd7e33a24ffc1935cf2165c489afb2a2c93e
Pekka Lehtola <pekka.lehtola98@gmail.com>
2021-04-06 14:01:28 +0300

    Exercise_work backup point

---
+++ OOP/Exercise_work/ammo_ui.py
@@ -0,0 +1,14 @@
+# File name: ammo_ui
+# Author: Pekka Lehtola
+# Description: Used for showing ammo elements in the screen.
+
+import pygame
+from config import *
+import time
+from ui import Ui
+
+class Ammo_ui(Ui):
+    def __init__(self):
+
+        super(Ammo_ui, self).__init__()
+        
---
+++ OOP/Exercise_work/heart_ui.py
@@ -0,0 +1,8 @@
+# File name: hearth_ui
+# Author: Pekka Lehtola
+# Description: Used for showing hearth elements in the screen.
+
+import pygame
+from config import *
+import time
+
